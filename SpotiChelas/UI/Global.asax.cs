2a783d59d2999682bebe98d3026c047b436c5cad
Tiago <a36842@alunos.isel.pt>
2013-07-05 16:25:20 +0000

    custom errors..

--- SpotiChelas/UI/Global.asax.cs
+++ SpotiChelas/UI/Global.asax.cs
@@ -17,7 +17,7 @@ namespace UI
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            //filters.Add(new HandleErrorAttribute());
+            filters.Add(new HandleErrorAttribute());
         }
 
         public static void RegisterRoutes(RouteCollection routes)
